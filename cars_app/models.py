5e27728093b721a70ebe2b6d417bff30de55f0f1
klaudiakryskiewicz <klaudia.sandra.kryskiewicz@gmail.com>
2020-08-21 14:09:01 +0200

    model funcs

--- cars_app/models.py
+++ cars_app/models.py
@@ -1,11 +1,17 @@
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.db.models import Sum
 
 
 class Car(models.Model):
     make = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
 
+    def ratings(self):
+        return Rating.objects.filter(car=self).count()
+
+    def average_rate(self):
+        return Rating.objects.aggregate(Sum('rate'))['rate']/self.ratings()
 
 class Rating(models.Model):
     car = models.ForeignKey(Car, on_delete=models.CASCADE)
