a31785b9054e760f2ae5314a8811f4391b7a24e0
klaudiakryskiewicz <klaudia.sandra.kryskiewicz@gmail.com>
2020-08-21 12:08:32 +0200

    models

--- cars_app/models.py
+++ cars_app/models.py
@@ -1,3 +1,16 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+
+class Rating(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    rate = models.IntegerField(validators=[MinValueValidator(1),
+                                           MaxValueValidator(5)])
+
+    class Meta:
+        verbose_name_plural = "ratings"
